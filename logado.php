266a04b64721b84e325387001a525fe5f1e90b47
Kleber Lima <mov_kleber@hotmail.com>
2020-08-27 14:10:44 -0300

    Adicionando Funcionalidades ao Login com PHP

---
+++ logado.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="pr-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="content">
+        <header class="content-header">
+            <h1>Login Militar</h1>
+        </header>
+        <?php include_once 'login.php';?>
+        <footer class="content-footer">
+            <p> Criado por Kleber Lima </p>
+        </footer>
+
+
+    </div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,66 @@
+<?php
+$matricula = filter_input(INPUT_POST, 'mat');
+$senha = filter_input(INPUT_POST, 'senha');
+error_reporting(E_ERROR);
+if (isset($matricula)) {
+    $usuarios = [
+        [
+            "nome" => "Kleber Lima",
+            "email" => "kleber@hotmail.com",
+            "matricula" => "301234567",
+            "senha" => "123456"
+        ],
+        [
+            "nome" => "Denise Souza",
+            "email" => "denise@hotmail.com",
+            "matricula" => "309876543",
+            "senha" => "654321"
+        ],
+    ];
+
+    foreach ($usuarios as $usuario) {
+        $matValida = $matricula === $usuario['matricula'];
+        $senhaValida = $senha === $usuario['senha'];
+
+        if ($matValida && $senhaValida) {
+            $_SESSION['error'] = null;
+            $_SESSION['usuario'] = $usuario['nome'];
+            $_SESSION['mat'] = $usuario['matricula'];
+            $_SESSION['e-mail'] = $usuario['email'];
+            header('Location: logado.php');
+            
+        }
+    }
+    if (!$_SESSION['usuario']) {
+        $_SESSION['error'] = ['Matrícula ou Senha inválida!'];
+    }
+}
+?>
+<main class="main-content">
+    <div id="main-box">
+        <div id="border-id">
+            <img src="https://images.unsplash.com/photo-1569242840510-9fe6f0112cee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="Imagem para Login">
+        </div>
+        <div>
+            <form action="#" id="formulario" method="POST">
+                <?php if ($_SESSION['error']): ?>
+                    <div class="error">
+                        <?php foreach ($_SESSION['error'] as $erro): ?>
+                            <p><?= $erro ?></p>
+                        <?php endforeach ?>    
+                    </div>
+                <?php endif ?>
+                <label for="nome">Matrícula</label>
+                <div><span><img src="https://img.icons8.com/ios-glyphs/30/000000/policeman-male.png"/></span><input type="text" name="mat" id="nome"></div>
+
+                <label for="senha">Senha</label>
+                <div><span><img src="https://img.icons8.com/ios-glyphs/30/000000/handcuffs.png"/></span><input type="password" name="senha" id="senha"></div>
+                <button>Entrar</button>
+            </form>
+        </div>
+
+
+    </div>
+</main>
+
+
